58dd61571781020c0675fdd712601bfd07deae2a
Charles-Emmanuel <charlesemmanuel.bray@gmail.com>
2021-05-15 19:03:57 +0200

    fix bug

--- src/main/java/fr/speedbooking/springboot/controller/UserController.java
+++ src/main/java/fr/speedbooking/springboot/controller/UserController.java
@@ -3,9 +3,7 @@ package fr.speedbooking.springboot.controller;
 import fr.speedbooking.springboot.model.User;
 import fr.speedbooking.springboot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
