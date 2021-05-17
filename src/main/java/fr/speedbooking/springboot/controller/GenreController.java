191f516c3c5baf2dbe4f2b4336fba4257a81e2a7
Emma <miem92@outlook.fr>
2021-05-17 22:23:35 +0200

    update genreController

--- src/main/java/fr/speedbooking/springboot/controller/GenreController.java
+++ src/main/java/fr/speedbooking/springboot/controller/GenreController.java
@@ -1,12 +1,15 @@
 package fr.speedbooking.springboot.controller;
 
-import fr.speedbooking.springboot.model.Book;
-import fr.speedbooking.springboot.repository.BookRepository;
+import fr.speedbooking.springboot.exception.RessourceNotFoundException;
+import fr.speedbooking.springboot.front.GenreInformation;
+import fr.speedbooking.springboot.front.UserInformation;
 import fr.speedbooking.springboot.repository.GenreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @CrossOrigin
 @RestController
@@ -14,6 +17,20 @@ import java.util.List;
 public class GenreController {
     @Autowired
     private GenreRepository genreRepository;
+    
+    @GetMapping("/allGenres")
+    public List<GenreInformation> getAllGenres(){
+        return genreRepository.findAll()
+                .stream()
+                .map(genre -> genre.parseToGenreInformation())
+                .collect(Collectors.toList());
+    }
 
+    @GetMapping("/findGenre/{id}")
+    public ResponseEntity<GenreInformation> getGenreById(@PathVariable Long id){
+        GenreInformation genre = genreRepository.findById(id)
+                .orElseThrow(() -> new RessourceNotFoundException("Genre does not exist at the id :" + id)).parseToGenreInformation();
+        return ResponseEntity.ok(genre);
+    }
 
 }
