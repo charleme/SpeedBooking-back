d8ffba5893888bef10f62305ddd7455073b0f08e
Charles-Emmanuel <charlesemmanuel.bray@gmail.com>
2021-05-25 12:28:55 +0200

    create genrebook update and get score

---
+++ src/main/java/fr/speedbooking/springboot/controller/GenreBookController.java
@@ -0,0 +1,61 @@
+package fr.speedbooking.springboot.controller;
+
+import fr.speedbooking.springboot.exception.RessourceNotFoundException;
+import fr.speedbooking.springboot.model.*;
+import fr.speedbooking.springboot.repository.BookRepository;
+import fr.speedbooking.springboot.repository.GenreBookRepository;
+import fr.speedbooking.springboot.repository.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/genrebook/")
+public class GenreBookController {
+    @Autowired
+    GenreBookRepository genreBookRepository;
+
+    @Autowired
+    GenreRepository genreRepository;
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @GetMapping("/getGenreBookScore/{idGenre}&{idBook}")
+    public int getScore(@PathVariable Long idGenre, @PathVariable Long idBook){
+        return genreBookRepository.getScore(idGenre, idBook);
+    }
+
+    @PutMapping("/updateScore/{idGenre}&{idBook}&{score}")
+    public ResponseEntity<Map<String, Integer>> updateScore(@PathVariable Long idGenre, @PathVariable Long idBook, @PathVariable int score){
+        GenreBook genreBook = genreBookRepository.getGenreBookByGenreIdAndBookId(idGenre, idBook);
+
+        genreBook.setScore(score);
+        genreBookRepository.save(genreBook);
+
+        Map<String, Integer> response = new HashMap<>();
+        response.put("score", genreBook.getScore());
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/createGenreBook/{idGenre}&{idBook}&{score}")
+    public ResponseEntity<Map<String, Boolean>> createGenreBook(@PathVariable Long idGenre, @PathVariable Long idBook, @PathVariable int score){
+        Genre genre = genreRepository.findById(idGenre)
+                .orElseThrow(() -> new RessourceNotFoundException("Genre does not exist at the id : " + idGenre));
+        Book book = bookRepository.findById(idBook)
+                .orElseThrow(() -> new RessourceNotFoundException("Book does not exist at the id : " + idBook));
+
+        GenreBook genreBook = new GenreBook(book,genre, score);
+
+        genreBookRepository.save(genreBook);
+
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("created", Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
+}
--- src/main/java/fr/speedbooking/springboot/repository/GenreBookRepository.java
+++ src/main/java/fr/speedbooking/springboot/repository/GenreBookRepository.java
@@ -1,9 +1,25 @@
 package fr.speedbooking.springboot.repository;
 
 import fr.speedbooking.springboot.model.GenreBook;
+import fr.speedbooking.springboot.model.UserBook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GenreBookRepository extends JpaRepository<GenreBook, Long> {
+    @Query("SELECT gb.score FROM GenreBook gb " +
+            "JOIN gb.idBook b " +
+            "JOIN gb.idGenre g " +
+            "WHERE b.idBook = :bookId " +
+            "AND g.idGenre = :genreId")
+    public int getScore(@Param("genreId") Long idGenre, @Param("bookId") Long idBook);
+
+    @Query("SELECT gb FROM GenreBook gb " +
+            "JOIN gb.idBook b " +
+            "JOIN gb.idGenre g " +
+            "WHERE b.idBook = :bookId " +
+            "AND g.idGenre = :genreId")
+    public GenreBook getGenreBookByGenreIdAndBookId(@Param("genreId") Long idGenre, @Param("bookId") Long idBook);
 }
